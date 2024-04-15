564d4a48c87ccc1bb93cea06c45bc160dff22543
qq <qq@qq.com>
2024-04-15 18:44:34 +0800

    3
    Signed-off-by: qq <qq@qq.com>

--- build/lede_x86/diy-part1.sh
+++ build/lede_x86/diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default          # 适合一键下载编译（smpackage目录）
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default          # 适合一键下载编译（smpackage目录）
 
 ./scripts/feeds clean                                                         # 清除编译临时文件
 ./scripts/feeds update -a                                                     # 更新插件源
